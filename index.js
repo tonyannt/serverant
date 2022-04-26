49f12db74e6e4f75c8259f0d95c21a2435b29208
tonyannt <tonyannt2510@gmail.com>
2022-04-26 08:25:27 -0500

    Configuración de sockets en el servidor, muestra mensaje de cliente conectado o desconectado

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+//Servidor de express compatible con sockets
 const app= express();
 //Esto lee el .env
 const path= require('path');
 require('dotenv').config();
+
+//Crear servidor de sockets how to use
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensajes de Sockets client: computadora resien conectada
+io.on('connection', client => {
+    console.log('cliente conectado');
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+     });
+  });
 
 //Conecta a pagina o dispositivo
 const publicPacth= path.resolve( __dirname, 'public');
@@ -10,7 +23,7 @@ const publicPacth= path.resolve( __dirname, 'public');
 //Conecta todo
 app.use(express.static(publicPacth));
 
-app.listen(process.env.PORT, (err) =>{//Escucha peticiones y cacha errores
+server.listen(process.env.PORT, (err) =>{//Escucha peticiones y cacha errores
     if(err) throw new Error(err); 
 
     console.log('servidor corriendo en puerto',process.env.PORT );
